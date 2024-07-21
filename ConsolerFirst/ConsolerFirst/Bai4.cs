c681b37da44a862495c7762a21c1d0d5140935e5
Nguyen Uyen <nguyenuyen552000@gmail.com>
2024-07-21 22:23:40 +0700

    add bai 4

---
+++ ConsolerFirst/ConsolerFirst/Bai4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolerFirst
+{
+    //Bài 4: Tính giai thừa của 1 số
+    internal class Bai4
+    {
+        public int TinhGiaiThuaCuaMotSo(int inputNumber )
+        {
+            int multiplication = 1;
+            for (int i = 1; i <= inputNumber; i++)
+            {
+                multiplication *= i;
+            }
+            return multiplication;
+        }
+    }
+}
---
+++ ConsolerFirst/ConsolerFirst/Bai5.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolerFirst
+{
+    //  Bài 5: Viết chương trình C# liệt kê tất cả các số nguyên tố nhỏ hơn n
+    internal class Bai5
+    {
+        public string ListNumberNguyenToBigNumberN(int n)
+        {
+            string listString = "";
+            for (int i = 1; i < n; i++)
+            {
+                if (checkSoNguyento(i))
+                {
+                    listString += i + ",";
+                }
+            }
+            return listString;
+        }
+        public bool checkSoNguyento(int inputNumbercheck)
+        {
+            bool IsNguyenTo = false;
+            if (inputNumbercheck < 3)
+            {
+                return IsNguyenTo;
+
+            }
+            int soLanNumberChiaHet = 0;
+            for (int i = 1; i < inputNumbercheck; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % 1 == 0 && i % j == 0)
+                    {
+                        soLanNumberChiaHet++;
+                    }
+                }
+                if (soLanNumberChiaHet == 1)
+                {
+                    IsNguyenTo = true;
+                }
+            }
+            return IsNguyenTo;
+        }
+    }
+}
